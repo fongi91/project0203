68cd9d1ce4da4bbfbe22e4cbaa29619ba661aa02
maengsky <mhaneul46@gmail.com>
2025-02-11 15:54:46 +0900

    Update EmployeeControllerAdvice.java
    로그인한 직원의 role 값을 가져오기 위한 코드 추가

--- project0203/demo/src/main/java/bio/controller/EmployeeControllerAdvice.java
+++ project0203/demo/src/main/java/bio/controller/EmployeeControllerAdvice.java
@@ -28,4 +28,40 @@ public class EmployeeControllerAdvice {
         } else { return null; }
     }
 
+    @ModelAttribute("isAdmin")
+    public boolean addisAdminToModel(HttpSession session) {
+        // 세션에서 로그인한 직원 정보를 가져옴
+        Employees employees = (Employees) session.getAttribute("employees");
+
+        // employees가 null이 아니고, role이 ADMIN과 같으면 true
+        boolean result = (employees != null) && (employees.getRole().equals(EmployeeRole.ADMIN));
+
+        log.info("isAdmin result: " + result);
+        return result;
+    }
+
+    @ModelAttribute("isHr")
+    public boolean addisHrToModel(HttpSession session) {
+        // 세션에서 로그인한 직원 정보를 가져옴
+        Employees employees = (Employees) session.getAttribute("employees");
+
+        // employees가 null이 아니고, role이 HR과 같으면 true
+        boolean result = (employees != null) && (employees.getRole().equals(EmployeeRole.HR));
+
+        log.info("isHr result: " + result);
+        return result;
+
+    }
+
+    @ModelAttribute("isLogistics")
+    public boolean addisLogisticsToModel(HttpSession session) {
+        // 세션에서 로그인한 직원 정보를 가져옴
+        Employees employees = (Employees) session.getAttribute("employees");
+
+        // employees가 null이 아니고, role이 LOGISTICS과 같은지 확인
+        boolean result = (employees != null) && (employees.getRole().equals(EmployeeRole.LOGISTICS));
+
+        log.info("isLogistics result: " + result);
+        return result;
+    }
 }
