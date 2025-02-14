8abe03d6fecb2c0ad64e6b1b1ba4ca2f3d484941
shimyunkyeng <ysim9857@gmail.com>
2025-02-14 10:37:31 +0900

    Update BioInvReceivingServiceImpl.java
    제품명 항목추가 관련 수정

--- project0203/demo/src/main/java/bio/service/BioInvReceivingServiceImpl.java
+++ project0203/demo/src/main/java/bio/service/BioInvReceivingServiceImpl.java
@@ -1,5 +1,6 @@
 package bio.service;
 
+import bio.domain.BioProduct;
 import bio.domain.BioInvReceiving;
 import bio.dto.BioInvReceivingDTO;
 import bio.dto.BioInvReceivingPageRequestDTO;
@@ -8,6 +9,8 @@ import bio.repository.BioInvReceivingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -25,19 +28,32 @@ public class BioInvReceivingServiceImpl implements BioInvReceivingService{
     private final ModelMapper modelMapper;
     private final BioInvReceivingRepository bioInvReceivingRepository;
 
+    @Autowired
+    public BioInvReceivingServiceImpl(ModelMapper modelMapper, BioInvReceivingRepository bioInvReceivingRepository, BioInvReceivingRepository bioInvReceivingRepository1) {
+        this.modelMapper = modelMapper;
+        this.bioInvReceivingRepository = bioInvReceivingRepository1;
+
+        modelMapper.addMappings(new PropertyMap<BioInvReceivingDTO, BioInvReceiving>() {
+            @Override
+            protected void configure() {
+                skip(destination.getBioProduct().getBioNo()); // bioNo는 매핑하지 않도록 처리
+            }
+        });
+
+        modelMapper.addMappings(new PropertyMap<BioInvReceivingDTO, BioProduct>() {
+            @Override
+            protected void configure() {
+                map(source.getProductCode(), destination.getProductCode()); // productCode 매핑
+                map(source.getProductName(), destination.getProductName()); // productName 매핑
+            }
+        });
+    }
+
     @Override
     public Long register(BioInvReceivingDTO bioInvReceivingDTO){
         BioInvReceiving bioInvReceiving = modelMapper.map(bioInvReceivingDTO, BioInvReceiving.class);
         Long receivingId = bioInvReceivingRepository.save(bioInvReceiving).getReceivingId();
         return receivingId;
-    }
-
-    @Override
-    public BioInvReceivingDTO readOne(Long receivingId){
-        Optional<BioInvReceiving> result = bioInvReceivingRepository.findById(receivingId);
-        BioInvReceiving bioInvReceiving = result.orElseThrow();
-        BioInvReceivingDTO bioInvReceivingDTO = modelMapper.map(bioInvReceiving, BioInvReceivingDTO.class);
-        return bioInvReceivingDTO;
     }
 
     @Override
@@ -54,16 +70,30 @@ public class BioInvReceivingServiceImpl implements BioInvReceivingService{
     }
 
     @Override
+    public BioInvReceivingDTO readOne(Long receivingId){
+        Optional<BioInvReceiving> result = bioInvReceivingRepository.findById(receivingId);
+        BioInvReceiving bioInvReceiving = result.orElseThrow();
+
+        BioInvReceivingDTO bioInvReceivingDTO = modelMapper.map(bioInvReceiving, BioInvReceivingDTO.class);
+        bioInvReceivingDTO.setProductName(bioInvReceiving.getProductName());
+        return bioInvReceivingDTO;
+    }
+
+    @Override
     public BioInvReceivingPageResponseDTO<BioInvReceivingDTO> list(BioInvReceivingPageRequestDTO bioInvReceivingPageRequestDTO){
         String[] types = bioInvReceivingPageRequestDTO.getTypes();
         String keyword = bioInvReceivingPageRequestDTO.getKeyword();
-        //추가
+
         String dateKeyword = bioInvReceivingPageRequestDTO.getDateKeyword();
         Pageable pageable = bioInvReceivingPageRequestDTO.getPageable("receivingId");
 
         Page<BioInvReceiving> result = bioInvReceivingRepository.searchAll(types, keyword, dateKeyword, pageable);
 
-        List<BioInvReceivingDTO> dtoList = result.getContent().stream().map(bioInvReceiving -> modelMapper.map(bioInvReceiving, BioInvReceivingDTO.class)).collect(Collectors.toList());
+        List<BioInvReceivingDTO> dtoList = result.getContent().stream()
+                .map(bioInvReceiving -> {BioInvReceivingDTO dto = modelMapper.map(bioInvReceiving, BioInvReceivingDTO.class);
+                                dto.setProductName(bioInvReceiving.getProductName());
+                                return dto;})
+                .collect(Collectors.toList());
 
         return BioInvReceivingPageResponseDTO.<BioInvReceivingDTO>withAll()
                 .bioInvReceivingPageRequestDTO(bioInvReceivingPageRequestDTO)
