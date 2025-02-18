afa37201c7da50728c56b69f54eb7aa75a535c99
shimyunkyeng <ysim9857@gmail.com>
2025-02-18 13:44:17 +0900

    Create BioInvStockServiceImpl.java
    재고관리 최초생성

---
+++ project0203/demo/src/main/java/bio/service/BioInvStockServiceImpl.java
@@ -0,0 +1,74 @@
+package bio.service;
+
+import bio.domain.BioInvStock;
+import bio.domain.BioProduct;
+import bio.dto.BioInvStockDTO;
+import bio.dto.BioInvStockPageRequestDTO;
+import bio.dto.BioInvStockPageResponseDTO;
+import bio.repository.BioInvStockRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional
+public class BioInvStockServiceImpl implements BioInvStockService{
+    private final ModelMapper modelMapper;
+    private final BioInvStockRepository bioInvStockRepository;
+
+    @Autowired
+    public BioInvStockServiceImpl(ModelMapper modelMapper, BioInvStockRepository bioInvStockRepository, BioInvStockRepository bioInvStockRepository1) {
+        this.modelMapper = modelMapper;
+        this.bioInvStockRepository = bioInvStockRepository1;
+
+        modelMapper.addMappings(new PropertyMap<BioInvStockDTO, BioInvStock>() {
+            @Override
+            protected void configure() {
+                skip(destination.getBioProduct().getBioNo()); // bioNo는 매핑하지 않도록 처리
+            }
+        });
+
+        modelMapper.addMappings(new PropertyMap<BioInvStockDTO, BioProduct>() {
+            @Override
+            protected void configure() {
+                map(source.getProductCode(), destination.getProductCode()); // productCode 매핑
+                map(source.getProductName(), destination.getProductName()); // productName 매핑
+            }
+        });
+    }
+
+    @Override
+    public BioInvStockPageResponseDTO<BioInvStockDTO> list(BioInvStockPageRequestDTO bioInvStockPageRequestDTO){
+        String[] types = bioInvStockPageRequestDTO.getTypes();
+        String keyword = bioInvStockPageRequestDTO.getKeyword();
+
+        String dateKeyword = bioInvStockPageRequestDTO.getDateKeyword();
+        Pageable pageable = bioInvStockPageRequestDTO.getPageable("StockId");
+
+        Page<BioInvStock> result = bioInvStockRepository.searchAll(types, keyword, dateKeyword, pageable);
+
+        List<BioInvStockDTO> dtoList = result.getContent().stream()
+                .map(bioInvStock -> {BioInvStockDTO dto = modelMapper.map(bioInvStock, BioInvStockDTO.class);
+                                dto.setProductName(bioInvStock.getProductName());
+                                return dto;})
+                .collect(Collectors.toList());
+
+        return BioInvStockPageResponseDTO.<BioInvStockDTO>withAll()
+                .bioInvStockPageRequestDTO(bioInvStockPageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
+    }
+}
